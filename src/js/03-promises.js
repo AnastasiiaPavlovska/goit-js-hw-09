516a77b368b39cc5030bec168c3716a564d2287e
AnastasiiaPavlovska <nastiapavlovska446@gmail.com>
2023-07-16 16:56:54 +0200

    Fix 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -9,33 +9,52 @@ const form = document.querySelector('.form');
 
 form.addEventListener('submit', onSubmit);
 
-function onSubmit(ev){
+function onSubmit(ev) {
   ev.preventDefault();
 
-  for (let i = 1, timeout = Number(form.delay.value); i <= Number(form.amount.value); i++, timeout+=Number(form.step.value)) {
+  if (form.delay.value < 0) {
+    Notify.failure('Please enter a number greater than zero for delay.', optNotiflx);
+    return; // Зупиняємо виконання функції у разі неправильного значення delay
+  }
+
+  if (form.step.value < 0) {
+    Notify.failure('Please enter a number greater than zero for step.', optNotiflx);
+    return; // Зупиняємо виконання функції у разі неправильного значення step
+  }
+
+  if (form.amount.value <= 0) {
+    Notify.failure('Please enter a number greater than zero for amount.', optNotiflx);
+    return; // Зупиняємо виконання функції у разі неправильного значення amount
+  }
+
+  for (
+    let i = 1, timeout = Number(form.delay.value);
+    i <= Number(form.amount.value);
+    i++, timeout += Number(form.step.value)
+  ) {
     createPromise(i, timeout)
-    .then(({ position, delay }) => {
-      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, optNotiflx);
-    })
-    .catch(({ position, delay }) => {
-      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, optNotiflx);
-    });
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, optNotiflx);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, optNotiflx);
+      });
   }
 }
 
 function createPromise(position, delay) {
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          resolve({position, delay});
-        }, delay);
-      })
-    } else {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          reject({position, delay});
-        }, delay);
-      })
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve({ position, delay });
+      }, delay);
+    });
+  } else {
+    return new Promise(reject => {
+      setTimeout(() => {
+        reject({ position, delay });
+      }, delay);
+    });
   }
 }
